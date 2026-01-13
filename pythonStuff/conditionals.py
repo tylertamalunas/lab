a4b2ac283bea46b6548db48778a1e9aaacd7deea
tylertamalunas <tamaluna@amazon.com>
2026-01-13 13:47:27 -0600

    basic python conditional

---
+++ pythonStuff/conditionals.py
@@ -0,0 +1,8 @@
+number = int(input("Type in a number: "))
+
+if number > 0:
+    print(f"{number} is positive.")
+elif number < 0:
+    print(f"{number} is negative.")
+else:
+    print(f"{number} is 0.")
