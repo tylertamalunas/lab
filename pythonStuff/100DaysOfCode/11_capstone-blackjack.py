496d5a745101efe6a00101097430ca47e4c38a89
tylertamalunas <tamaluna@amazon.com>
2026-02-20 13:59:59 -0600

    wrote intial deal, score and deal functions

--- pythonStuff/100DaysOfCode/11_capstone-blackjack.py
+++ pythonStuff/100DaysOfCode/11_capstone-blackjack.py
@@ -12,6 +12,17 @@ Design:
     - Ace changes value based on total score of hand (1 or 11)
     - 
 """
+"""
+STEPS:
+D    1. Deal player hand and dealer hand
+D    1b. if player or dealer or both have blackjack, game is immediately over, determine winner
+    2. tell hands and player score, ask if user wants to 'hit' or 'stay' (no splitting since theres no bets)
+    3a. if 'hit', add card to player hand and revert back to step 2.
+    3b. if player score is over 21, player loses
+    3c. if stay, deal dealer hand and show score.
+    4a. if dealer hand is 17 or higher, stay
+    4b. if dealer hand is 16 or lower, hit and revert back to step 4a. 
+"""
 
 import random
 
@@ -31,12 +42,35 @@ def calc_score(hand, deck, cards):
 
 cards = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
 deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-print("Welcome to Blackjack.")
 run = True
 while run:
+    print("Welcome to Blackjack.")
     player_hand = []
     dealer_hand = []
     draw_card(player, deck, cards)
     draw_card(player, deck, cards)
     draw_card(dealer, deck, cards)
     draw_card(dealer, deck, cards)
+    player_score = calc_score(player_hand, deck, cards)
+    dealer_score = calc_score(dealer_hand, deck, cards)
+    print(f"The dealer has [{dealer_hand[0], Hidden}]")
+    print(f"You have {player_hand}")
+    if dealer_score == 21 and player_score == 21:
+        print("DRAW. Dealer and Player both have Blackjack.")
+        print(f"Dealer: {dealer_hand}")
+        print(f"You: player_hand}")
+    elif player_score == 21:
+        print("WINNER. Player has Blackjack!")
+        print(f"You: {player_hand}")
+    elif dealer_score == 21:
+        print("LOSE. Dealer has Blackjack!")
+        print(f"Dealer: {dealer_hand}")
+    action = 'hit'
+    else:
+        while player_score <=21
+            action = input(f"You have {player_score}. Do you want to 'hit' or 'stay'? ")
+            if action == hit:
+                draw_card(player, deck, cards)
+                player_score = calc_score(player_hand, deck, cards)
+            else:
+
